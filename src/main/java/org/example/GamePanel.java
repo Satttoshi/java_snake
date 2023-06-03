2c0c56e8dd0c577600d64bd74b25022f5c5fe6a9
Josh <joshuastolle445@gmail.com>
2023-06-04 01:03:05 +0200

    render snake

--- src/main/java/org/example/GamePanel.java
+++ src/main/java/org/example/GamePanel.java
@@ -19,8 +19,8 @@ public class GamePanel extends JPanel implements ActionListener {
     final int y[] = new int[GAME_UNITS];
     int bodyParts = 6;
     int applesEaten;
-    int AppleX;
-    int AppleY;
+    int appleX;
+    int appleY;
     char direction = 'R';
     boolean running = false;
     Timer timer;
@@ -39,22 +39,55 @@ public class GamePanel extends JPanel implements ActionListener {
         newApple();
         running = true;
         timer = new Timer(DELAY, this);
+        timer.start();
 
     }
 
     public void paintComponent(Graphics g){
-
+        super.paintComponent(g);
+        draw(g);
     }
 
     public void draw(Graphics g){
 
+        for (int i = 0; i < SCREEN_HEIGHT/UNIT_SIZE; i++){
+            g.drawLine(i * UNIT_SIZE, 0, i * UNIT_SIZE, SCREEN_HEIGHT);
+            g.drawLine(0, i * UNIT_SIZE, SCREEN_WIDTH, i * UNIT_SIZE);
+        }
+        g.setColor(Color.red);
+        g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
+
+        for(int i = 0; i < bodyParts; i++) {
+            if(i == 0) {
+                g.setColor(Color.green);
+                g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+            }
+            else {
+                g.setColor(new Color(45, 180, 0));
+                g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+            }
+        }
+
     }
 
     public void newApple(){
-
+        appleX = random.nextInt(SCREEN_WIDTH/UNIT_SIZE) * UNIT_SIZE;
+        appleY = random.nextInt(SCREEN_HEIGHT/UNIT_SIZE) * UNIT_SIZE;
     }
 
     public void move(){
+
+        for(int i = bodyParts; i > 0; i--){
+            x[i] = x[i-1];
+            y[i] = y[i-1];
+        }
+
+        switch (direction) {
+            case 'U' -> y[0] = y[0] - UNIT_SIZE;
+            case 'D' -> y[0] = y[0] + UNIT_SIZE;
+            case 'L' -> x[0] = x[0] - UNIT_SIZE;
+            case 'R' -> x[0] = x[0] + UNIT_SIZE;
+        }
 
     }
 
@@ -73,7 +106,12 @@ public class GamePanel extends JPanel implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        // TODO Auto-generated method stub
+        if(running){
+            move();
+            checkApple();
+            checkCollisions();
+        }
+        repaint();
     }
 
     public class MyKeyAdapter extends KeyAdapter {
