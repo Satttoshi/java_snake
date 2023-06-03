c62c4de28c48279537181b4116725d96d5453422
Josh <joshuastolle445@gmail.com>
2023-06-04 01:05:04 +0200

    add collisions

--- src/main/java/org/example/GamePanel.java
+++ src/main/java/org/example/GamePanel.java
@@ -97,6 +97,34 @@ public class GamePanel extends JPanel implements ActionListener {
 
     public void checkCollisions(){
 
+        // Checks if head collides with body
+        for (int i = bodyParts; i > 0; i--){
+            if ((x[0] == x[i]) && (y[0] == y[i])) {
+                running = false;
+                break;
+            }
+        }
+
+        // Checks if head touches left border
+        if (x[0] < 0) {
+            running = false;
+        }
+
+        // Checks if head touches right border
+        if (x[0] > SCREEN_WIDTH) {
+            running = false;
+        }
+
+        // Checks if head touches top border
+        if (y[0] < 0) {
+            running = false;
+        }
+
+        // Checks if head touches bottom border
+        if (y[0] > SCREEN_HEIGHT) {
+            running = false;
+        }
+
     }
 
     public void gameOver(Graphics g){
