f2345b858682000552c47ee4ef3e762b8333284f
Josh <joshuastolle445@gmail.com>
2023-06-03 19:00:10 +0200

    init repo

---
+++ src/main/java/org/example/AsciiPanel.java
@@ -0,0 +1,35 @@
+package org.example;
+
+public class AsciiPanel {
+    private static final int WIDTH = 80;
+    private static final int HEIGHT = 24;
+    private char[][] panel;
+
+    public AsciiPanel() {
+        panel = new char[HEIGHT][WIDTH];
+    }
+
+    public void clear() {
+        for (int y = 0; y < HEIGHT; y++) {
+            for (int x = 0; x < WIDTH; x++) {
+                panel[y][x] = ' ';
+            }
+        }
+    }
+
+    public void setChar(int x, int y, char ch) {
+        if (x >= 0 && x < WIDTH && y >= 0 && y < HEIGHT) {
+            panel[y][x] = ch;
+        }
+    }
+
+    public void display() {
+        for (int y = 0; y < HEIGHT; y++) {
+            for (int x = 0; x < WIDTH; x++) {
+                System.out.print(panel[y][x]);
+            }
+            System.out.println();
+        }
+    }
+}
+
